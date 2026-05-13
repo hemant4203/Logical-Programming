021a266700dec43ccfe256892156b24305164e1a
hemant4203 <85121389+hemant4203@users.noreply.github.com>
2026-05-13 12:51:26 +0530

    Implement everyNth function to return every nth character

---
+++ CodingBat/Warmup-1/Everynth.java
@@ -0,0 +1,8 @@
+public String everyNth(String str, int n) {
+  StringBuilder sb = new StringBuilder();
+  for(int i=0;i<str.length(); i+=n) {
+    sb.append(str.charAt(i));
+  }
+  str=sb.toString();
+  return str;
+}
