bdd44102e626c5472a00c61fc2df2ea623f66113
hemant4203 <85121389+hemant4203@users.noreply.github.com>
2026-05-19 10:50:23 +0530

    Add double23 method to check for double 2s or 3s

---
+++ CodingBat/Array-1/Double23.java
@@ -0,0 +1,18 @@
+public boolean double23(int[] nums) {
+  if(nums.length<2)
+   return false;
+   
+  int count2=0;
+  int count3=0;
+  
+  for(int i=0; i<nums.length; i++) {
+    if(nums[i]==2)
+     count2++;
+    else if(nums[i]==3)
+     count3++;
+  }
+  
+  if(count2==2 || count3==2)
+   return true;
+  return false;
+}
