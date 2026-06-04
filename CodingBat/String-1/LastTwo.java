e005a5846b4e53ca4229f391326a28ba09bbc5c8
hemant4203 <85121389+hemant4203@users.noreply.github.com>
2026-06-04 14:42:52 +0530

    Update LastTwo.java

--- CodingBat/String-1/LastTwo.java
+++ CodingBat/String-1/LastTwo.java
@@ -1,11 +1,7 @@
 public String lastTwo(String str) {
   if(str.length()<2) return str;
-  if(str.length() == 2){
-    String first = str.substring(0,1);
-    String last = str.substring(1);
-    return last+first;
-  }
-  String first = str.substring(str.length()-1);
-  String last = str.substring(str.length()-2,str.length()-1);
-  return str.substring(0,str.length()-2)+first+last;
+  String body = str.substring(0,str.length()-2);
+  char first = str.charAt(str.length()-1);
+  char last = str.charAt(str.length()-2);
+  return body+first+last;
 }
