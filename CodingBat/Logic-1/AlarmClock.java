01be6ce4a49cd08ab394453c09e6765d55401e80
hemant4203 <85121389+hemant4203@users.noreply.github.com>
2026-06-12 15:11:05 +0530

    Add alarmClock method for vacation and weekday logic

---
+++ CodingBat/Logic-1/AlarmClock.java
@@ -0,0 +1,8 @@
+public String alarmClock(int day, boolean vacation) {
+  if(vacation){
+    if(day>0 && day<6) return "10:00";
+    return "off";
+  }
+  if(day>0 && day<6) return "7:00";
+  return "10:00";
+}
